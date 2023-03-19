1167bd63d1c4f026ea2365eff91cb60a533506a2
theflag505 <hamdan.mobeen@gmail.com>
2023-03-19 06:31:00 -0700

    assignment 4 update

--- cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -14,38 +14,38 @@ import com.cst438.domain.EnrollmentRepository;
 
 
 public class GradebookServiceMQ extends GradebookService {
-	
-	@Autowired
-	RabbitTemplate rabbitTemplate;
-	
-	@Autowired
-	EnrollmentRepository enrollmentRepository;
-	
-	@Autowired
-	Queue gradebookQueue;
-	
-	
-	public GradebookServiceMQ() {
-		System.out.println("MQ grade book service");
-	}
-	
-	// send message to grade book service about new student enrollment in course
-	@Override
-	public void enrollStudent(String student_email, String student_name, int course_id) {
-		 
-		// TODO 
-		// create EnrollmentDTO and send to gradebookQueue
-		
-		System.out.println("Message send to gradbook service for student "+ student_email +" " + course_id);  
-		
-	}
-	
-	@RabbitListener(queues = "registration-queue")
-	public void receive(CourseDTOG courseDTOG) {
-		System.out.println("Receive enrollment :" + courseDTOG);
 
-		//TODO 
-		// for each student grade in courseDTOG,  find the student enrollment entity, update the grade and save back to enrollmentRepository.
-	}
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    EnrollmentRepository enrollmentRepository;
+
+    @Autowired
+    Queue gradebookQueue;
+
+
+    public GradebookServiceMQ() {
+        System.out.println("MQ grade book service");
+    }
+
+    // send message to grade book service about new student enrollment in course
+    @Override
+    public void enrollStudent(String student_email, String student_name, int course_id) {
+        EnrollmentDTO enroll = new EnrollmentDTO(student_email, student_name, course_id);
+        rabbitTemplate.convertAndSend(gradebookQueue.getName(), enroll);
+        System.out.println("Message send to gradbook service for student " + student_email + " " + course_id);
+    }
+
+    @RabbitListener(queues = "registration-queue")
+    public void receive(CourseDTOG courseDTOG) {
+        System.out.println("Receive enrollment :" + courseDTOG);
+        for (CourseDTOG.GradeDTO grade : courseDTOG.grades) {
+            String email = grade.student_email;
+            Enrollment student = enrollmentRepository.findByEmailAndCourseId(email, courseDTOG.course_id);
+            student.setCourseGrade(grade.grade);
+            enrollmentRepository.save(student);
+        }
+    }
 
 }
